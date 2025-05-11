1586647433ffb7e7d2be8c698f7ad7c19061e212
Elijah Creighton <89100976+CreightonEli@users.noreply.github.com>
2025-05-10 21:43:52 -0400

    new cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Info, Star, StarHalf } from "@phosphor-icons/react";
+import { Info, CalendarBlank, Star, StarHalf } from "@phosphor-icons/react";
 
 export default function Cards(props) {
     const starSize = 20;
@@ -21,25 +21,66 @@ export default function Cards(props) {
     return (
         <>
             {props.poster_path != null && (
-                <div className="card">
-                    <img src={"https://image.tmdb.org/t/p/w500/" + props.poster_path} className="movie-cover" />
-                    <div className="text-content">
-                        <h2 className="title">
-                            {(props.title || props.name)} 
-                        </h2>
-                        {(props.release_date && props.release_date.split('-')[0])}
-                        {(props.first_air_date && props.first_air_date.split('-')[0])}
-                        <div className="cards-rating">{starArray[parseInt(props.vote_average)]}</div>
+                <div className="card" key={props.id}>
+                    <div className="card-top">
                         {props.title && (
                             <Link to={`/movies/${props.id}`}>
-                                <button>More info<Info size={18} /></button>
+                                <img src={"https://image.tmdb.org/t/p/w500/" + props.poster_path} className="movie-cover" />
                             </Link>
                         )}
                         {props.name && (
                             <Link to={`/shows/${props.id}`}>
-                                <button>More info<Info size={18} /></button>
+                                <img src={"https://image.tmdb.org/t/p/w500/" + props.poster_path} className="movie-cover" />
                             </Link>
                         )}
+                        {/* <img src={"https://image.tmdb.org/t/p/w500/" + props.poster_path} className="movie-cover" /> */}
+                        {/* <img src={"https://image.tmdb.org/t/p/w500/" + props.poster_path} className="movie-cover" /> */}
+                        {/* <div className="text-content">
+                            <h2 className="title">
+                                {(props.title || props.name)} 
+                            </h2>
+                            {(props.release_date && props.release_date.split('-')[0])}
+                            {(props.first_air_date && props.first_air_date.split('-')[0])}
+                            <div className="cards-rating">{starArray[parseInt(props.vote_average)]}</div>
+                            {props.title && (
+                                <Link to={`/movies/${props.id}`}>
+                                    <button>More info<Info size={18} /></button>
+                                </Link>
+                            )}
+                            {props.name && (
+                                <Link to={`/shows/${props.id}`}>
+                                    <button>More info<Info size={18} /></button>
+                                </Link>
+                            )}
+                        </div> */}
+                    </div>
+                    <div className="card-bottom">
+                        <h2 className="title">
+                            {props.title && (
+                                <Link to={`/movies/${props.id}`}>
+                                    {props.title}
+                                </Link>
+                            )}
+                            {props.name && (
+                                <Link to={`/shows/${props.id}`}>
+                                    {props.name}
+                                </Link>
+                            )}
+                        </h2>
+                        <p className="quick-info">
+                            <span className="year">
+                                {/* <CalendarBlank size={16} /> */}
+                                {(props.release_date && props.release_date.split('-')[0])}
+                                {(props.first_air_date && props.first_air_date.split('-')[0])}
+                            </span>
+                            <Star size={14} weight="fill" />
+                            <span className="rating">
+                                {Math.round(props.vote_average * 10) / 10}
+                            </span>
+                            <span className="type">
+                                {(props.title && 'Movie') || (props.name && 'TV Show')}
+                            </span>
+                        </p>
                     </div>
                 </div>
             )}
