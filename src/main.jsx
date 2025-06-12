973250082c243d3199c2ed1979d834adef63b9a0
Elijah Creighton <89100976+CreightonEli@users.noreply.github.com>
2025-06-12 02:04:13 -0400

    Update main.jsx
    switched to client-side routing with HashRouter since we really should've been doing that from the start. B)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // <-- Change here
 import './scss/style.scss';
 import Header from './components/Header';
 import App from './App.jsx';
@@ -10,13 +10,13 @@ import { HeaderVisibilityProvider } from './context/HeaderVisibilityContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <HeaderVisibilityProvider>
         <ScrollToTop />
         <Header />
         <App />
         <Footer />
       </HeaderVisibilityProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
