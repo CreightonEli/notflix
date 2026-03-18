7fa4979f63e67c1dfea24b581f892b9613d9d372
Elijah Creighton <89100976+CreightonEli@users.noreply.github.com>
2026-03-18 01:46:44 -0400

    More animations and some attention to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,35 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import logoFull from '../assets/logo_full.png'; // Use relative path
+
 export default function Footer() {
+    const [isSmallScreen, setIsSmallScreen] = useState(window.matchMedia("(max-width: 950px)").matches);
+    
     return (
         <footer>
             <div className="footer-content">
-                <p>© 2025 Nullflix. All rights reserved.</p>
+                <div className="footer-top">
+                    <div className="footer-logo">
+                        <Link to={`/`}>
+                            <img 
+                                className="logo" 
+                                src={logoFull} 
+                                alt="Nullflix Logo" 
+                            />
+                        </Link>
+                    </div>
+                    <div className="footer-list">
+                        <Link to={`/`}>Home</Link>
+                        <Link to={`/lists`}>My List</Link>
+                        <Link to={`/history`}>History</Link>
+                        <Link to={`/settings`}>Settings</Link>
+                        {/* <Link to={`/about`}>About</Link> */}
+                    </div>
+                </div>
+                <div className="footer-info">
+                </div>
                 <p>Made with 💜 by <a href={"https://creightoneli.github.io"}>Elijah C.</a></p>
+                <p>© 2025 Nullflix. All rights reserved.</p>
             </div>
         </footer>
     );
